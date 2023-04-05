e8ebb808295626b780baafdeceddcbaadfd967c4
Paulo Vinicius <pauloviniciustavaressouza@gmail.com>
2023-04-05 03:17:02 -0300

    create: section banner

---
+++ src/Components/Hero.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import BgShape from '../images/hero/hero-bg.png';
+import HeroCar from '../images/hero/main-car.png';
+import { Link } from 'react-router-dom';
+
+export const Hero = () => {
+  return (
+    <>
+      <section className="hero-section">
+        <div className="container">
+          <img className="bg-shape" src={BgShape} alt="bg-shape" />
+
+          <div className="hero-content">
+            <div className="hero-content__text">
+              <h4>Plan your trip now</h4>
+              <h1>
+                Save <span>big</span> with our car rental
+              </h1>
+              <p>
+                Rent the car of your dreams. Unbeatable prices, unlimited miles,
+                flexible pick-up options and much more.
+              </p>
+              <div className="hero-content__text__btns">
+                <Link className="hero-content__text__btns__book-ride" to="/">
+                  Book Ride
+                </Link>
+                <Link className="hero-content__text__btns__learn-more" to="/">
+                  Learn More
+                </Link>
+              </div>
+            </div>
+
+            {/* img */}
+
+            <img
+              className="hero-content__car-img"
+              src={HeroCar}
+              alt="car-img"
+            />
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Hero } from '../Components/Hero';
 
 export const Home = () => {
   return (
-    <div>
-      <h1>HOME</h1>
-    </div>
+    <>
+      <Hero />
+    </>
   );
 };
