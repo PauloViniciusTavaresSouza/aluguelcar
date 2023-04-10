4b5ea9a41a87e2c4dbf66af1e757086cd7858864
Paulo Vinicius <pauloviniciustavaressouza@gmail.com>
2023-04-10 01:16:09 -0300

    init: style tabBox

--- src/Components/CarBox.jsx
+++ src/Components/CarBox.jsx
@@ -3,7 +3,6 @@ import React, { useState } from 'react';
 export const CarBox = ({ data, carID }) => {
   const [carLoad, setCarLoad] = useState(true);
 
-  console.log(data[carID].map((car, id) => car.img));
   return (
     <>
       {data[carID].map((car, id) => {
@@ -11,8 +10,55 @@ export const CarBox = ({ data, carID }) => {
           <div key={id} className="box-cars">
             <div className="pick-car">
               {carLoad && <span className="loader"></span>}
-              <img src={car.img} alt="" onLoad={() => setCarLoad(false)} />
-              <h1>qualquer coisa</h1>
+              <img
+                style={{ display: carLoad ? 'none' : 'block' }}
+                src={car.img}
+                alt="car-img"
+                onLoad={() => setCarLoad(false)}
+              />
+            </div>
+            <div className="pick-description">
+              <div className="pick-description__price">
+                <span>${car.price}/ rent per day</span>
+              </div>
+              <div className="pick-description__table">
+                <div className="pick-description__table__col">
+                  <span>Model</span>
+                  <span>{car.model}</span>
+                </div>
+                <div className="pick-description__table__col">
+                  <span>Mark</span>
+                  <span>{car.mark}</span>
+                </div>
+
+                <div className="pick-description__table__col">
+                  <span>Year</span>
+                  <span>{car.year}</span>
+                </div>
+
+                <div className="pick-description__table__col">
+                  <span>Doors</span>
+                  <span>{car.doors}</span>
+                </div>
+
+                <div className="pick-description__table__col">
+                  <span>AC</span>
+                  <span>{car.air}</span>
+                </div>
+
+                <div className="pick-description__table__col">
+                  <span>Transmission</span>
+                  <span>{car.transmission}</span>
+                </div>
+
+                <div className="pick-description__table__col">
+                  <span>Fuel</span>
+                  <span>{car.fuel}</span>
+                </div>
+              </div>
+              <a className="cta-btn" href="#booking-section">
+                Reserve Now
+              </a>
             </div>
           </div>
         );
